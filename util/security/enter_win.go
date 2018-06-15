c513d78de957f0b2ebf3b9d962f4d553425d3345
Rangding Zhang <love-nankai@163.com>
2018-06-15 15:35:50 +0800

    *

---
+++ util/security/enter_win.go
@@ -0,0 +1,46 @@
+// +build windows
+
+/**	Thanks for: https://github.com/slonzok/getpass	*/
+
+package security
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	"syscall"
+	"unsafe"
+	"fmt"
+)
+
+var (
+	getStdHandle,
+	getConsoleMode,
+	setConsoleMode *syscall.LazyProc
+)
+
+func init() {
+	lib := syscall.NewLazyDLL("Kernel32.dll")
+	getStdHandle = lib.NewProc("GetStdHandle")
+	getConsoleMode = lib.NewProc("GetConsoleMode")
+	setConsoleMode = lib.NewProc("SetConsoleMode")
+}
+
+func EnterWithPrompt(prompt string) string {
+	fmt.Println(prompt + ":")
+	return Enter()
+}
+
+func Enter() string {
+	handle, _, _ := getStdHandle.Call(uintptr(^uint(10) + 1))
+	var mode uint
+	getConsoleMode.Call(handle, uintptr(unsafe.Pointer(&mode)))
+	newMode := mode & ^uint(4)
+	setConsoleMode.Call(handle, uintptr(newMode))
+	passwd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	setConsoleMode.Call(handle, uintptr(mode))
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimSpace(passwd)
+}
