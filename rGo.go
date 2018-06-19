91e9c2d1fe2e10627722b4ff687a8166f01d5582
Rangding Zhang <love-nankai@163.com>
2018-06-19 17:08:46 +0800

    Update rGo.go

--- rGo.go
+++ rGo.go
@@ -25,35 +25,47 @@ func main() {
 		os.Exit(1)
 	}
 
-	for _, config := range configs{
+	for _, config := range configs {
 		if config.User == "" {
 			config.User = util.Input.User
 		}
 		if config.Password == "" && len(config.KeyFiles) == 0 {
 			config.Password = util.Input.Password
 		}
+		cmds := util.Input.Cmds
+		headMsg := fmt.Sprintf("%s%s", config, cmds)
+		if util.Input.Sh != "" {
+			shContent, err := ioutil.ReadFile(util.Input.Sh)
+			{
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+			}
+			headMsg = fmt.Sprintf("%s%s", config, util.Input.Sh)
+			cmds = string(shContent)
+		}
 		if strings.ToLower(config.Host) == "localhost" || config.Host == "127.0.0.1" {
-			go localExec(util.Input.Cmd)
+			go localExec(headMsg, cmds)
 			continue
 		}
 		sshClient, err := ssh.NewClient(&config)
 		if err != nil {
-			fmt.Printf("%s%s\n%s\n", config, util.Input.Cmd, err.Error())
+			fmt.Printf("%s%s\n%s\n", config, util.Input.Cmds, err.Error())
 			continue
 		}
-		headMsg := fmt.Sprintf("%s%s", config, util.Input.Cmd)
-		go remoteExec(sshClient, headMsg, util.Input.Cmd)
+		go remoteExec(sshClient, headMsg, cmds)
 	}
 	time.Sleep(time.Second * time.Duration(util.Input.Duration))
 }
 
-func localExec(cmds string)  {
+func localExec(headMsg, cmds string) {
 	result, err := cmd.Run(cmds)
 	if err != nil {
-		fmt.Printf("[localhost]%s\n%s\n", cmds, err.Error())
+		fmt.Printf("%s\n%s\n", headMsg, err.Error())
 		return
 	}
-	fmt.Printf("[localhost]%s\n%s\n", cmds, result)
+	fmt.Printf("%s\n%s\n", headMsg, result)
 }
 
 func remoteExec(sshClient *ssh.Client, headMsg, cmds string) {
