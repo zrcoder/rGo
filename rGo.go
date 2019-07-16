11a00781043c7031f7dfc62856b13342e3d1835f
Rangding Zhang <love-nankai@163.com>
2019-07-16 22:22:09 +0800

    Update rGo.go

--- rGo.go
+++ rGo.go
@@ -1,77 +1,91 @@
 package main
 
-import (
-	"fmt"
+import (	
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 	"time"
+	
 	"github.com/DingHub/rGo/util"
 	"github.com/DingHub/rGo/util/ssh"
 	"github.com/DingHub/rGo/util/cmd"
-	"strings"
-	"os"
-	"io/ioutil"
 )
 
+const lineSep = "\n"
+ 
+var logger = log.New(os.Stdout, "", log.Lshortfile|log.LstdFlags)
+ 
 func main() {
 	configData, err := ioutil.ReadFile("config.json")
 	if err != nil {
-		fmt.Println(err)
+		logger.Println(err)
 		os.Exit(1)
 	}
 	var configs []ssh.Config
 	err = json.Unmarshal(configData, &configs)
 	if err != nil {
-		fmt.Println(err)
+		logger.Println(err)
 		os.Exit(1)
 	}
-
+ 
 	for _, config := range configs {
-		if config.User == "" {
-			config.User = util.Input.User
-		}
-		if config.Password == "" && len(config.KeyFiles) == 0 {
-			config.Password = util.Input.Password
-		}
-		cmds := util.Input.Cmds
-		headMsg := fmt.Sprintf("%s%s", config, cmds)
-		if util.Input.Sh != "" {
-			shContent, err := ioutil.ReadFile(util.Input.Sh)
-			if err != nil {
-+				fmt.Println(err)
-+				os.Exit(1)
- 			}
-			headMsg = fmt.Sprintf("%s%s", config, util.Input.Sh)
-			cmds = string(shContent)
-		}
-		if strings.ToLower(config.Host) == "localhost" || config.Host == "127.0.0.1" {
-			go localExec(headMsg, cmds)
-			continue
-		}
-		sshClient, err := ssh.NewClient(&config)
-		if err != nil {
-			fmt.Printf("%s%s\n%s\n", config, util.Input.Cmds, err.Error())
-			continue
-		}
-		go remoteExec(sshClient, headMsg, cmds)
+		go func() {
+			if config.User == "" {
+				config.User = util.Input.User
+			}
+			if config.Password == "" && len(config.KeyFiles) == 0 {
+				config.Password = util.Input.Password
+			}
+			cmds := util.Input.Cmds
+			headMsg := fmt.Sprintf("%s%s:", config, cmds)
+			if util.Input.Sh != "" {
+				shContent, err := ioutil.ReadFile(util.Input.Sh)
+				if err != nil {
+					logger.Println(err)
+					os.Exit(1)
+				}
+				headMsg = fmt.Sprintf("%s%s:", config, util.Input.Sh)
+				cmds = string(shContent)
+			}
+			if strings.ToLower(config.Host) == "localhost" || config.Host == "127.0.0.1" {
+				localExec(headMsg, cmds)
+			} else {
+				sshClient, err := ssh.NewClient(config)
+				if err != nil {
+					logger.Printf("%s\n%s\n", headMsg, err.Error())
+				} else {
+					remoteExec(sshClient, headMsg, cmds)
+				}
+			}
+		}()
 	}
 	time.Sleep(time.Second * time.Duration(util.Input.Duration))
 }
-
+ 
 func localExec(headMsg, cmds string) {
 	result, err := cmd.Run(cmds)
 	if err != nil {
-		fmt.Printf("%s\n%s\n", headMsg, err.Error())
+		logger.Println(headMsg, err.Error())
 		return
 	}
-	fmt.Printf("%s\n%s\n", headMsg, result)
+	logger.Printf("%s\n%s\n", headMsg, result)
 }
-
+ 
 func remoteExec(sshClient *ssh.Client, headMsg, cmds string) {
-	result, stderr, err := sshClient.Run(cmds)
+	stdout, stderr, err := sshClient.Run(cmds)
 	defer sshClient.Close()
-	if err != nil {
-		fmt.Printf("%s\n%s\n%s\n", headMsg, stderr, err.Error())
-		return
+ 
+	headMsg = strings.TrimRight(headMsg, lineSep) + lineSep
+	if stderr != "" {
+		headMsg += "stderr: " + stderr + lineSep
 	}
-	fmt.Printf("%s\n%s\n", headMsg, result)
+	if err != nil {
+		headMsg += "error: " + err.Error() + lineSep
+	} else {
+		headMsg += strings.TrimRight(stdout, lineSep) + lineSep
+	}
+	logger.Println(headMsg)
 }
