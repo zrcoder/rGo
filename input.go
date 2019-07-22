315a3d6d633985dd3902e8e843efa483f8be3536
Rangding Zhang <love-nankai@163.com>
2019-07-22 15:34:18 +0800

    Update input.go

--- input.go
+++ input.go
@@ -16,29 +16,23 @@ var Input struct {
 }
 
 func init() {
-	const (
-		uUsage = "if there is no \"user\" field of some record in config.json,\n" +
-			"value of this option will be used"
-		pUsage = "if there is no \"paasword\" or \"key_files\" field of some records in config.json,\n" +
-			"we can use this option and then enter the paasword"
-	)
-	var (
-		help          = false
-		useEnteredPwd = false
-	)
-	flag.StringVar(&Input.Cmds, "c", "", "commands (can be separated with \";\")")
-	flag.StringVar(&Input.Sh, "sh", "", "shell script file to be executed")
-	flag.StringVar(&Input.User, "u", "", uUsage)
-	flag.BoolVar(&useEnteredPwd, "p", false, pUsage)
-	flag.BoolVar(&help, "help", false, "show help info")
-	flag.Parse()
+	flag.StringVar(&Input.Cmds, "c", "", "")
+	flag.StringVar(&Input.Sh, "sh", "", "")
+	flag.StringVar(&Input.User, "u", "", "")
+	useEnteredPwd := flag.Bool("p", false, "")
 
-	if help || (Input.Cmds == "" && Input.Sh == "") {
-		printHelpInfo()
-		os.Exit(0)
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, helpInfo)
 	}
 
-	if useEnteredPwd {
+	flag.Parse()
+
+	if Input.Cmds == "" && Input.Sh == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *useEnteredPwd {
 		prompt := "Please enter the password"
 		Input.Password = security.EnterWithPrompt(prompt)
 	}
@@ -63,7 +57,3 @@ const helpInfo = `  ------------------------------------------------------------
 	if there is no "paasword" or "key_files" field of some records in config.json,
 	we can use this flag and then enter the paasword
 `
-
-func printHelpInfo() {
-	fmt.Println(helpInfo)
-}
